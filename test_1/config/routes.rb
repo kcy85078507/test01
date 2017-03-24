07f2caaf3d7d1f3bb79b6a7438a7e66aa2008455
chae yeon <kcy85078507@likelion.org>
2017-03-25 00:13:31 +0900

    last

--- test_1/config/routes.rb
+++
@@ -1,5 +0,0 @@
-Rails.application.routes.draw do
-	get 'email/email_form' => 'email#email_form'
-	post 'email/email_send' => 'email#email_send'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
